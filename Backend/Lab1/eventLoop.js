faeec66a6e540d98a9d35d64d0c44a7846bd6ca9
akarshb23 <akarshb23@gmail.com>
2026-07-31 14:31:46 +0530

    dom events making

--- Backend/Lab1/eventLoop.js
+++ Backend/Lab1/eventLoop.js
@@ -17,3 +17,16 @@ setImmediate(()=>{
 },2000);
 
 console.log("This is the end point of my code")
+
+new Promise((resolve,reject)=>{
+    let success = true ;
+    if(success){
+        resolve("Data loaded successfully")
+    }else{
+        reject("Data loading failed")
+    }
+}).then((message)=>{
+    console.log(message);
+}).catch((message)=>{
+    console.log(message);
+})
