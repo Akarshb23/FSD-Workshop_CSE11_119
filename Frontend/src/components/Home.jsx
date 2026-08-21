c9bb03c116f448df10f1364143d1ddf8783d7410
akarshb23 <akarshb23@gmail.com>
2026-08-21 13:32:30 +0530

    committed at 21/8/2026

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -2,18 +2,34 @@ import React from "react";
 import books from "../assets/Books";
 import "./Home.css";
 
-
 function Home() {
   return (
-    <div>
-      <h1>Welcome to the book store</h1>
-      <div>
-        {books.map((book)=>){
-          
-        }}
+    <div className="home">
+      <h1 className="heading">Welcome to the Book Store</h1>
+
+      <div className="book-container">
+        {books.map((book) => (
+          <div className="book-card" key={book.id}>
+            <img
+              className="book-image"
+              src={book.poster}
+              alt={book.title}
+            />
+
+            <div className="book-details">
+              <h2>{book.title}</h2>
+              <p>Author: {book.author}</p>
+              <p>Price: ₹{book.price}</p>
+
+              <button className="buy-btn">
+                Buy Now
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
